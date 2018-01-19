19536b058787e0b67713d6e290728231a74e8969
Marcin Kumorek <marcin@kalohq.com>
2018-01-19 23:16:52 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,33 +1,38 @@
-const express = require('express');
-const models = require('./models');
-const expressGraphQL = require('express-graphql');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const schema = require('./schema/schema');
+require('dotenv').config()
+const express = require('express')
 
-const app = express();
+const models = require('./models')
+const expressGraphQL = require('express-graphql')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const schema = require('./schema/schema')
+
+const app = express()
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@ds263137.mlab.com:63137/lyrical-graphql'
 if (!MONGO_URI) {
-  throw new Error('You must provide a MongoLab URI');
+	throw new Error('You must provide a MongoLab URI')
 }
 
-mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
-mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+mongoose.Promise = global.Promise
+mongoose.connect(MONGO_URI, {useMongoClient: true}).then(
+	() => console.log('Connected to MongoLab instance.'),
+	error => console.error('Error connecting to MongoLab:', error)
+)
 
-app.use(bodyParser.json());
-app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true
-}));
+app.use(bodyParser.json())
+app.use(
+	'/graphql',
+	expressGraphQL({
+		schema,
+		graphiql: true,
+	})
+)
 
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('../webpack.config.js');
-app.use(webpackMiddleware(webpack(webpackConfig)));
+const webpackMiddleware = require('webpack-dev-middleware')
+const webpack = require('webpack')
+const webpackConfig = require('../webpack.config.js')
+app.use(webpackMiddleware(webpack(webpackConfig)))
 
-module.exports = app;
+module.exports = app
