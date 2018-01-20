73e92d22922490d6c777e0f897ea5e2f7bc27916
Marcin Kumorek <kumeek@gmail.com>
2018-01-20 14:53:18 +0100

    Convert Home to spaces, add navigation

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Home = props => {
-	return (
-		<section>
-			<header>
-				<h1>Lyrical</h1>
-			</header>
-			{props.children}
-		</section>
-	)
+  return (
+    <section>
+      <header>
+        <h1>Lyrical</h1>
+      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/song/add">Add song</Link>
+          </li>
+        </ul>
+      </nav>
+      {props.children}
+    </section>
+  )
 }
 
 export default Home
