7d03a720c58012f7411a7e97c44ccf3c5715582a
Marcin Kumorek <marcin@kalohq.com>
2018-01-20 12:16:40 +0100

    Add SongForm

--- client/index.js
+++ client/index.js
@@ -5,6 +5,7 @@ import ApolloClient from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
 
 import Home from './components/Home'
+import SongForm from './components/SongForm'
 import SongList from './components/SongList'
 
 const client = new ApolloClient({})
@@ -15,6 +16,7 @@ const Root = () => {
 			<Router history={hashHistory}>
 				<Route path="/" component={Home}>
 					<IndexRoute component={SongList} />
+					<Route path="song/add" component={SongForm} />
 				</Route>
 			</Router>
 		</ApolloProvider>
