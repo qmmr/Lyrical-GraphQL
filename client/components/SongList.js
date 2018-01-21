0a26ff764c0852c2411b2657b7e3e18356555bc2
Marcin Kumorek <kumeek@gmail.com>
2018-01-21 13:26:29 +0100

    Use deleteSong mutation in Song component

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react'
-import { graphql } from 'react-apollo'
+import { compose, graphql } from 'react-apollo'
 
 import { query } from '../queries/getSongs'
+import { mutation } from '../mutations/deleteSong'
+
+const Song = ({ id, mutate, title }) => {
+  const handleClick = event => {
+    event.preventDefault()
+    mutate({
+      variables: { id },
+    }).then(data => console.log(data))
+  }
+
+  return (
+    <li
+      key={id}
+      style={{
+        padding: '0 1em',
+        border: '1px solid #444',
+        marginBottom: '1em',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}>
+      <p>{title}</p>
+      <span onClick={handleClick}>
+        <i className="material-icons">delete</i>
+      </span>
+    </li>
+  )
+}
 
 class SongList extends Component {
   render() {
@@ -9,10 +38,16 @@ class SongList extends Component {
     return (
       <div>
         <h2>SongList</h2>
-        {loading ? <div>Loading...</div> : <ul>{songs.map(({ id, title }) => <li key={id}>{title}</li>)}</ul>}
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <ul style={{ display: 'flex', flexDirection: 'column' }}>
+            {songs.map(props => <Song key={props.id} {...props} mutate={this.props.mutate} />)}
+          </ul>
+        )}
       </div>
     )
   }
 }
 
-export default graphql(query)(SongList)
+export default compose(graphql(query), graphql(mutation))(SongList)
