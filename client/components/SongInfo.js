999946b7efed5727a278258354f2796177bedaec
Marcin Kumorek <kumeek@gmail.com>
2018-01-21 20:42:06 +0100

    Show lyrics in SongInfo

--- client/components/SongInfo.js
+++ client/components/SongInfo.js
@@ -1,15 +1,28 @@
 /* eslint-disable react/prop-types */
+import LyricForm from './LyricForm'
 import React from 'react'
 import { graphql } from 'react-apollo'
 import { query } from '../queries/song'
 
-export const SongInfo = ({ data }) => {
+export const SongInfo = ({ data, params }) => {
   const { loading, song } = data
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
 
   return (
     <section>
       <h1>Song Info</h1>
-      {loading ? <div>Loading...</div> : <h2>Title: {song.title}</h2>}
+      <h2>Title: {song.title}</h2>
+      <ul>
+        {song.lyrics.map(lyric => (
+          <li key={lyric.id}>
+            <p>{lyric.content}</p>
+          </li>
+        ))}
+      </ul>
+      <LyricForm id={params.id} />
     </section>
   )
 }
